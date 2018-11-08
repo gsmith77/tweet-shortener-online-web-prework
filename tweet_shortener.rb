dd8acd69042183765c21ae8a5cd76ba870148b32
grant smith <grantsmith1256@gmail.com>
2018-11-08 17:50:28 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require "pry"
+
+def dictionary(hash)
+  dictionary = {"hello" => "hi", "to,two,too" => "2", "for,four" => "4", "be" => "b", "you" => "u", "at"=> "@", "and" => "&"}
+    dictionary.keys.map do |key, word, value|
+    if key == word
+      value
+    end
+  end
+  hash
+end
+      
+def word_substituter(string)
+  binding.pry
+end
